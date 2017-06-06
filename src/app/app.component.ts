acab5657b1d0b3e83b2afe5a5cb882b57d62131c
Kevin Mui <kmui2@wisc.edu>
2017-06-05 21:54:58 -0500

    adding buttons for upload and train start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,20 @@ import { NeatapticService } from './services/neataptic/neataptic.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-   LOCV_percent = 0.8;
-   num_points = 100;
-   num_input_neurons = 10;
-   num_hidden_neurons = 14;
-   num_output_neurons = 1;
-   training_options = {
-    log: 10,
-    error: 0.0000000001,
-    iterations: 1000,
-    rate: 0.3
+  config = {
+     LOCV_percent : 0.8,
+     num_points : 100,
+     num_input_neurons : 10,
+     num_hidden_neurons : 14,
+     num_output_neurons : 1,
+     training_options : {
+      log: 10,
+      error: 0.0000000001,
+      iterations: 1000,
+      rate: 0.3
+    }
   }
+  file: any;
   
   constructor (private neatapticService: NeatapticService) {
     
@@ -31,16 +34,12 @@ export class AppComponent implements OnInit {
 
     var comp = this;
     input.onchange = function() {
-      const config = {
-        LOCV_percent: comp.LOCV_percent,
-        num_points: comp.num_points,
-        num_input_neurons: comp.num_input_neurons,
-        num_hidden_neurons: comp.num_hidden_neurons,
-        num_output_neurons: comp.num_output_neurons,
-        training_options: comp.training_options
-      }
-      comp.neatapticService.parseAndTrain(input.files[0], config);
+      comp.file = input.files[0];
     };
+  }
+  
+  train() {
+    this.neatapticService.parseAndTrain(this.file, this.config);
   }
 
 } 
